e29164601fc7a6989ea5720f5248644090be03b5
jackburt12 <contact@jackburt.me.uk>
2020-01-15 19:44:55 +0000

    Added a building, Trash script, player movement, background looping, weather movement with player

---
+++ Assets/Scripts/Environment/Objects/TrashCan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCan : MonoBehaviour
+{
+
+    Sprite[] sprites;
+
+    void Awake()
+    {
+        sprites = Resources.LoadAll<Sprite>("Sprites/World/Trash");
+
+        int randNum = Random.Range(0,sprites.Length);
+ 
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[randNum];
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller2D;
+
+    public float runSpeed = 20f;
+
+    private float horizontalVelocity = 0f;
+    private bool jump = false;
+    private bool crouch = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalVelocity = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump")) {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch")) {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch")) {
+            crouch = false;
+        }
+        
+    }
+
+    void FixedUpdate () {
+
+        controller2D.Move(horizontalVelocity * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+    }
+}
