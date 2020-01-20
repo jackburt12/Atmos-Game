291348cdc100cbd1aa991cf9029d129df4847926
jackburt12 <contact@jackburt.me.uk>
2020-01-20 12:12:15 +0000

    Added pedestrians (only guys in suits initially)

---
+++ Assets/Scripts/Environment/NPCs/Pedestrian.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Pedestrian : MonoBehaviour
+{
+
+    public bool goLeft = true;
+
+    float movespeed;
+
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+
+        movespeed = 0.04f;
+        float randNum = Random.Range(-0.01f,0.01f);
+        movespeed = movespeed + (randNum);
+
+        if(goLeft) {
+            transform.localScale = new Vector3(-1,1,1);
+            movespeed = -movespeed;
+        }
+
+        int randOrder = (int)Random.Range(-5,3);
+        if(randOrder == 0) {
+            randOrder = -1;
+        }
+
+        gameObject.GetComponent<SortingGroup>().sortingOrder = randOrder;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + movespeed, transform.position.y);
+        
+        float distance = player.transform.position.x - gameObject.transform.position.x;
+        if(Mathf.Abs(distance) > 20f) {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Environment/NPCs/SpawnPedestrians.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPedestrians : MonoBehaviour
+{
+    //how many pedestrians should spawn
+    //a spawn rate of 1 means one every 10 seconds
+    public int spawnRate;
+
+    GameObject[] pedestriansList;
+    GameObject pedestrian;
+    public GameObject player;
+
+    float rate;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pedestriansList = Resources.LoadAll<GameObject>("Prefabs/Characters/Pedestrians");
+
+        rate = 10 / spawnRate;
+        timer = rate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            SpawnPedestrian();
+            timer = rate;
+        }
+    }
+
+    void SpawnPedestrian() {
+
+        pedestrian = pedestriansList[(int)Random.Range(0,pedestriansList.Length)];
+
+
+        int rand = Random.Range(0, 2);
+        if(rand == 0) {
+            rand = -1;
+        }
+
+        Debug.Log("Ped direction: " + rand);
+        Vector3 positionVector = new Vector3((player.transform.position.x + rand * 15), -3, player.transform.position.z);
+        GameObject spawnedPedestrian = Instantiate(pedestrian, positionVector, Quaternion.identity, gameObject.transform);
+        spawnedPedestrian.transform.parent = gameObject.transform.parent;
+
+        if(rand<0) {
+            spawnedPedestrian.GetComponent<Pedestrian>().goLeft = false;
+        }
+
+    }
+
+}
